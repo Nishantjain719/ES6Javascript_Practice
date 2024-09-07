c0007a95c80630d22e0a632bfa43dfb70250f6c1
Nishant Jain <Nishant.Jain@in.niit.com>
2024-09-07 14:45:49 +0530

    commitnew

--- main.js
+++ main.js
@@ -18,3 +18,36 @@ var capitalizeName = function (name) {
 }
 var capitalNames = names.map(capitalizeName).join(",");
 
+// filter
+names.filter(function (name) {
+    return name.length > 4;
+})
+
+//oop
+var Vehicle = function (wheelCount, color) {
+    this.wheels = wheelCount;
+    this.color = color;
+}
+
+Vehicle.prototype.honk = function () {
+
+}
+
+var Car = function (color) {
+    this.wheels = 4;
+    this.color = color;
+}
+Car.prototype = new Vehicle()
+Car.prototype.honk = function () {
+    console.log('beep beep!')
+}
+Car.prototype.move = function (dist) {
+    console.log('Moving car:', dist)
+}
+Vehicle.prototype.toString = function () { }
+var car = new Car('red');
+car.move(100)
+
+//Instance of the Vehicle class
+var cycle = new Vehicle(2, 'black');
+car.honk();
